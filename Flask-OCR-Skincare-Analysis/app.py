8032a35c2b71224c988bb3a367f000370feb1268
Asrini Muhsin <150633663+Asrin111@users.noreply.github.com>
2024-12-06 02:40:42 +0800

    Update app.py

--- Flask-OCR-Skincare-Analysis/app.py
+++ Flask-OCR-Skincare-Analysis/app.py
@@ -128,4 +128,5 @@ def predict():
 
 # Menjalankan aplikasi Flask
 if __name__ == '__main__':
-    app.run(debug=True)
+     port = int(os.environ.get('PORT', 8080))  
+    app.run(host='0.0.0.0', port=port, debug=True)
